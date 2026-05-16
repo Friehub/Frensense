187253bca0dd3d4593e377eee703e280b76ccd7a
Friehub Developers <action@github.com>
2026-05-16 20:04:10 +0100

    refactor: decompose analyze_block to satisfy clippy::too_many_lines (fmt)

--- src/semantics/data_flow/tracking.rs
+++ src/semantics/data_flow/tracking.rs
@@ -37,112 +37,185 @@ impl<'a> DataFlowAnalyzer<'a, '_> {
         let block_range = super::normalization::Range::from(node);
 
         for op in self.context.semantic_ops {
-            // Only process ops within the current block's range
             match op {
                 SemanticOp::Binding { name, value_range } => {
-                    if value_range.start_byte >= block_range.start_byte
-                        && value_range.end_byte <= block_range.end_byte
-                    {
-                        let v_node = self.node_at(*value_range);
-                        registry.register_symbol(name, v_node);
-                        let val_code = &self.current_source[v_node.start_byte()..v_node.end_byte()];
-
-                        let origin = if source_re.is_match(name) || source_re.is_match(val_code) {
-                            Some(super::TaintOrigin::UserInput)
-                        } else {
-                            self.resolve_taint(v_node, &registry)
-                        };
-
-                        if let Some(o) = origin {
-                            if let Some((obj, prop)) = name.split_once('.') {
-                                registry.taint_field(obj, prop, o);
-                            } else {
-                                registry.taint(name, o);
-                            }
-                        }
-
-                        self.propagate_object_taint(name, v_node, &mut registry);
-                    }
+                    self.process_binding(name, *value_range, block_range, source_re, &mut registry);
                 }
                 SemanticOp::Assignment {
                     target,
                     value_range,
                 } => {
-                    if value_range.start_byte >= block_range.start_byte
-                        && value_range.end_byte <= block_range.end_byte
-                    {
-                        let v_node = self.node_at(*value_range);
-                        let val_code = &self.current_source[v_node.start_byte()..v_node.end_byte()];
-
-                        let origin = if source_re.is_match(target) || source_re.is_match(val_code) {
-                            Some(super::TaintOrigin::UserInput)
-                        } else {
-                            self.resolve_taint(v_node, &registry)
-                        };
-
-                        if let Some(o) = origin {
-                            if let Some((obj, prop)) = target.split_once('.') {
-                                registry.taint_field(obj, prop, o);
-                            } else {
-                                registry.taint(target, o);
-                            }
-                        }
-
-                        self.propagate_object_taint(target, v_node, &mut registry);
-                    }
+                    self.process_assignment(
+                        target,
+                        *value_range,
+                        block_range,
+                        source_re,
+                        &mut registry,
+                    );
                 }
                 SemanticOp::Call {
                     function_name,
                     args,
                     range,
                 } => {
-                    if range.start_byte >= block_range.start_byte
-                        && range.end_byte <= block_range.end_byte
-                    {
-                        let arg_nodes: Vec<Node> = args.iter().map(|r| self.node_at(*r)).collect();
-                        let call_node = self.node_at(*range);
-                        advisories.extend(self.analyze_call(
-                            function_name,
-                            &arg_nodes,
-                            call_node,
-                            source_re,
-                            sink_re,
-                            rule,
-                            &mut registry,
-                        ));
+                    if let Some(call_advisories) = self.process_call(
+                        function_name,
+                        args,
+                        *range,
+                        block_range,
+                        source_re,
+                        sink_re,
+                        rule,
+                        &mut registry,
+                    ) {
+                        advisories.extend(call_advisories);
                     }
                 }
                 SemanticOp::EnterBlock(body_range) => {
-                    if body_range.start_byte > block_range.start_byte
-                        && body_range.end_byte < block_range.end_byte
-                        && self.depth < self.max_depth
-                    {
-                        let body_node = self.node_at(*body_range);
-                        registry.push_scope();
-                        let sub_analyzer = DataFlowAnalyzer::with_depth(
-                            self.context,
-                            self.current_source,
-                            self.current_tree,
-                            self.current_file_path,
-                            self.current_file_id,
-                            body_node,
-                            self.depth + 1,
-                            self.max_depth,
-                        );
-                        advisories.extend(sub_analyzer.analyze_block(
-                            body_node,
-                            source_re,
-                            sink_re,
-                            rule,
-                            registry.clone(),
-                        ));
-                        registry.pop_scope();
+                    if let Some(sub_advisories) = self.process_enter_block(
+                        *body_range,
+                        block_range,
+                        source_re,
+                        sink_re,
+                        rule,
+                        &mut registry,
+                    ) {
+                        advisories.extend(sub_advisories);
                     }
                 }
             }
         }
 
         advisories
+    }
+
+    fn process_binding(
+        &self,
+        name: &'a str,
+        value_range: super::normalization::Range,
+        block_range: super::normalization::Range,
+        source_re: &Regex,
+        registry: &mut TaintRegistry<'a>,
+    ) {
+        if value_range.start_byte >= block_range.start_byte
+            && value_range.end_byte <= block_range.end_byte
+        {
+            let v_node = self.node_at(value_range);
+            registry.register_symbol(name, v_node);
+            let val_code = &self.current_source[v_node.start_byte()..v_node.end_byte()];
+
+            let origin = if source_re.is_match(name) || source_re.is_match(val_code) {
+                Some(super::TaintOrigin::UserInput)
+            } else {
+                self.resolve_taint(v_node, registry)
+            };
+
+            if let Some(o) = origin {
+                if let Some((obj, prop)) = name.split_once('.') {
+                    registry.taint_field(obj, prop, o);
+                } else {
+                    registry.taint(name, o);
+                }
+            }
+
+            self.propagate_object_taint(name, v_node, registry);
+        }
+    }
+
+    fn process_assignment(
+        &self,
+        target: &'a str,
+        value_range: super::normalization::Range,
+        block_range: super::normalization::Range,
+        source_re: &Regex,
+        registry: &mut TaintRegistry<'a>,
+    ) {
+        if value_range.start_byte >= block_range.start_byte
+            && value_range.end_byte <= block_range.end_byte
+        {
+            let v_node = self.node_at(value_range);
+            let val_code = &self.current_source[v_node.start_byte()..v_node.end_byte()];
+
+            let origin = if source_re.is_match(target) || source_re.is_match(val_code) {
+                Some(super::TaintOrigin::UserInput)
+            } else {
+                self.resolve_taint(v_node, registry)
+            };
+
+            if let Some(o) = origin {
+                if let Some((obj, prop)) = target.split_once('.') {
+                    registry.taint_field(obj, prop, o);
+                } else {
+                    registry.taint(target, o);
+                }
+            }
+
+            self.propagate_object_taint(target, v_node, registry);
+        }
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    fn process_call(
+        &self,
+        function_name: &'a str,
+        args: &[super::normalization::Range],
+        range: super::normalization::Range,
+        block_range: super::normalization::Range,
+        source_re: &Regex,
+        sink_re: &Regex,
+        rule: &dyn GenSenseRule,
+        registry: &mut TaintRegistry<'a>,
+    ) -> Option<Vec<Advisory>> {
+        if range.start_byte >= block_range.start_byte && range.end_byte <= block_range.end_byte {
+            let arg_nodes: Vec<Node> = args.iter().map(|r| self.node_at(*r)).collect();
+            let call_node = self.node_at(range);
+            Some(self.analyze_call(
+                function_name,
+                &arg_nodes,
+                call_node,
+                source_re,
+                sink_re,
+                rule,
+                registry,
+            ))
+        } else {
+            None
+        }
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    fn process_enter_block(
+        &self,
+        body_range: super::normalization::Range,
+        block_range: super::normalization::Range,
+        source_re: &Regex,
+        sink_re: &Regex,
+        rule: &dyn GenSenseRule,
+        registry: &mut TaintRegistry<'a>,
+    ) -> Option<Vec<Advisory>> {
+        if body_range.start_byte > block_range.start_byte
+            && body_range.end_byte < block_range.end_byte
+            && self.depth < self.max_depth
+        {
+            let body_node = self.node_at(body_range);
+            registry.push_scope();
+            let sub_analyzer = DataFlowAnalyzer::with_depth(
+                self.context,
+                self.current_source,
+                self.current_tree,
+                self.current_file_path,
+                self.current_file_id,
+                body_node,
+                self.depth + 1,
+                self.max_depth,
+            );
+            let sub_advisories =
+                sub_analyzer.analyze_block(body_node, source_re, sink_re, rule, registry.clone());
+            registry.pop_scope();
+            Some(sub_advisories)
+        } else {
+            None
+        }
     }
 
     #[allow(clippy::too_many_arguments)]
