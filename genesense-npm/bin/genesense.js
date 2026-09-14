e9e6f1eea5e27fe149cf695744dbbcceb4e5baf7
Friehub Developers <action@github.com>
2026-09-14 10:52:23 +0100

    fix(release): add --allow-dirty for cargo publish, fix genesense npm path
    - cargo publish fails because Cargo.lock is dirty (npm install changes it)
    - genesense-npm/bin/genesense.js still referenced bin/frensense.js
      (renamed to bin/run.js in previous commit)

--- genesense-npm/bin/genesense.js
+++ genesense-npm/bin/genesense.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 console.warn("\nWARNING: The 'genesense' CLI is deprecated. Please use 'frensense' instead.\n");
-require('@friehub/frensense/bin/frensense.js');
+require('@friehub/frensense/bin/run.js');
